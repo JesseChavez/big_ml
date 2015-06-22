2002fc79da9b6bf358154529d53837457e8475b9
Dany Chavez <danyfiee@yahoo.com>
2015-06-22 15:22:52 +1000

    changed to australia url

--- lib/big_ml/util/config.rb
+++ lib/big_ml/util/config.rb
@@ -2,9 +2,9 @@ module BigML
   module Util
     module Config
       # The endpoint that will be used to connect if none is set
-      BIGML_PROD_ENDPOINT    = "https://bigml.io/andromeda"
+      BIGML_PROD_ENDPOINT    = "https://au.bigml.io/andromeda"
       # The endpoint that will be used when dev_mode is enabled
-      BIGML_DEV_ENDPOINT     = "https://bigml.io/dev/andromeda"
+      BIGML_DEV_ENDPOINT     = "https://au.bigml.io/dev/andromeda"
       # The username if none is set
       DEFAULT_BIGML_USERNAME = nil
       # The api key if none is set
